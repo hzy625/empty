fa8a08af0ad5e5a1b9942916b3d51ad44a43cf7a
Zhuoyang Han <81492838+hzy625@users.noreply.github.com>
2021-04-13 11:23:59 -0400

    update installed apps

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'adopt',
+    'map',
+    'sightings',
     ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
